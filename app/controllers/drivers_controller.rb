46f49e46f757c4fe8759f2698d42456b665b31d3
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-08 20:44:16 -0700

    adds driver functionality and views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -13,4 +13,46 @@ class DriversController < ApplicationController
     end
   end
 
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver.id) 
+      return
+    else 
+      render :edit 
+      return
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, available: true)
+  end
+
 end
