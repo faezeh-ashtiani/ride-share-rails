61ec9f4797129d6ba4cc61098bd0ca543e42b29e
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-12 09:02:58 -0700

    adds tests for edit, update and delete for trips controller

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -55,12 +55,12 @@ describe TripsController do
       #trip_hash = {passenger_id: @passenger.id}
 
       # Act-Assert
-      # Ensure that there is a change of 1 in Driver.count
+      # Ensure that there is a change of 1 in Trip.count
       expect {
         post passenger_trip_path(@passenger.id)
       }.must_differ 'Trip.count', 1
       # Assert
-      # Find the newly created Driver, and check that all its attributes match what was given in the form data
+      # Find the newly created Trip, and check that all its attributes match what was given in the form data
       # Check that the controller redirected the user
     must_redirect_to trip_path(Trip.last.id)
     
@@ -107,14 +107,153 @@ describe TripsController do
   end
 
   describe "edit" do
-    # Your tests go here
+    it "responds with success when getting the edit page for an existing, valid trip" do
+      # Arrange
+      # Ensure there is an existing trip saved
+      trip_id = Trip.last.id
+  
+      # Act
+      get "/trips/#{trip_id}/edit"
+      
+      # Assert
+      must_respond_with :success
+    end
+  
+    it "responds with redirect when getting the edit page for a non-existing trip" do
+      # Arrange
+      # Ensure there is an invalid id that points to no trip
+      invalid_id = -1
+      # Act
+      get "/trips/#{invalid_id}/edit"
+      # Assert
+      must_respond_with :not_found
+    end
+
   end
 
   describe "update" do
-    # Your tests go here
+    it "can update an existing trip with valid information accurately, and redirect" do
+      # Arrange
+      # Ensure there is an existing trip saved
+      # Assign the existing trip's id to a local variable
+      # Set up the form data
+      trip_id = Trip.last.id
+
+      driver_2 = Driver.create({
+        name: "Faezeh Ashtiani",
+        vin: "YTREWDFGHGFDSDF",
+        available: true
+      })
+  
+      trip_hash = {
+        trip: {
+          rating: 3,
+          cost: 1500,
+          driver_id: driver_2.id
+        }
+      }
+  
+      # Act-Assert
+      # Ensure that there is no change in Trip.count
+      expect {
+        patch trip_path(trip_id), params: trip_hash
+      }.wont_change "Trip.count"
+  
+      # Assert
+      # Use the local variable of an existing trip's id to find the trip again, and check that its attributes are updated
+      trip = Trip.find_by(id: trip_id)
+      expect(trip.rating).must_equal trip_hash[:trip][:rating]
+      expect(trip.cost).must_equal trip_hash[:trip][:cost]
+  
+      # Check that the controller redirected the user
+      must_redirect_to trip_path(trip_id)
+    end
+  
+    it "does not update any trip if given an invalid id, and responds with a 404" do
+      # Arrange
+      # Ensure there is an invalid id that points to no trip
+      invalid_id = -1
+      # Set up the form data
+      trip_hash = {
+        trip: {
+          rating: 3,
+          cost: 1500,
+          driver_id: @driver.id
+        }
+      }
+  
+      # Act-Assert
+      # Ensure that there is no change in Trip.count
+      expect {
+        patch trip_path(invalid_id), params: trip_hash
+      }.wont_change "Trip.count"
+  
+      # Assert
+      # Check that the controller gave back a 404
+      must_respond_with :not_found
+    end
+  
+    it "does not create a trip if the form data violates Trip validations, and responds with a redirect" do
+      # Note: This will not pass until ActiveRecord Validations lesson
+      # Arrange
+      # Ensure there is an existing trip saved
+      # Assign the existing trip's id to a local variable
+      # Set up the form data so that it violates trip validations
+  
+      trip_id = Trip.last.id
+  
+      trip_hash = {
+        trip: {
+          rating: 3,
+          cost: 1500,
+          driver_id: @driver.id
+        }
+      }
+      trip_hash[:trip][:rating] = nil
+      # Act-Assert
+      # Ensure that there is no change in Trip.count
+      expect {
+        patch trip_path(trip_id), params: trip_hash
+      }.wont_change "Trip.count"
+  
+      # Assert
+      # Check that the controller redirects
+      must_respond_with :bad_request
+    end
   end
 
   describe "destroy" do
-    # Your tests go here
+    it "destroys the trip instance in db when trip exists, then redirects" do
+      # Arrange
+      # Ensure there is an existing trip saved
+      trip_id = Trip.last.id
+  
+      # Act-Assert
+      # Ensure that there is a change of -1 in Trip.count
+      expect {
+        delete trip_path(trip_id)
+      }.must_differ "Trip.count", -1
+  
+      # Assert
+      # Check that the controller redirects
+      must_redirect_to root_path
+    end
+  
+    it "does not change the db when the trip does not exist, then responds with a 404 " do
+      # Arrange
+      # Ensure there is an invalid id that points to no trip
+      invalid_id = -1
+  
+      # Act-Assert
+      # Ensure that there is no change in Trip.count
+      expect {
+        delete trip_path(invalid_id)
+      }.wont_change "Trip.count"
+  
+      # Assert
+      # Check that the controller responds or redirects with whatever your group decides
+      must_respond_with :not_found
+  
+    end
   end
 end
